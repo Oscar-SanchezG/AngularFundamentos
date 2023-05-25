bcd6aa34fdbbb3a7b76098a46900785b2c06e09f
Oscar Sanchez <oscar.e.sanchez.g@gmail.com>
2023-05-25 05:12:19 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { People } from './people.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  widthImg = 10;
+  color: string[] = [
+    'verde',
+    'azul',
+    'rojo'
+  ];
+  name = 'bambosan';
+  age = 27;
+  img = 'https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg';
+  btnDisable = true;
+  person = {
+    name: 'Oscar',
+    age: 27,
+    avatar: "https://static.vecteezy.com/system/resources/previews/002/275/847/non_2x/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg"
+  }
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
+
+  names: string[] = ['bambosan', 'pedro', 'arturo'];
+  newName = '';
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+
+  peopleArray: People[] = [
+    {
+        firstname: 'Freddy',
+        lastname: 'Vega',
+        age: 35,
+        image: './assets/img/foto1.png'
+    },
+    {
+        firstname: 'Nicolas',
+        lastname: 'Molina',
+        age: 29,
+        image: './assets/img/foto2.png',
+        sexo: 'masculino'
+    },
+    {
+        firstname: 'Ángela',
+        lastname: 'Ocando',
+        age: 30,
+        image: './assets/img/foto3.png'
+    }
+  ];
+
+  toggleButton(){
+    this.btnDisable = !this.btnDisable;
+  }
+  addAge(){
+    this.person.age += 1;
+  }
+
+  onSroll(event: Event){
+    const element = event.target as HTMLDivElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = "";
+  }
+
+  deleteName(index: number){
+    this.names.splice(index, 1);
+  }
+  onRegister() {
+    console.log(this.register);
+  }
 }
---
+++ src/app/people.model.ts
@@ -0,0 +1,7 @@
+export interface People {
+  firstname: string;
+  lastname: string;
+  age: number;
+  image: string;
+  sexo?: string; //le decimos al modelo que puedo o no venir la opcion
+}
